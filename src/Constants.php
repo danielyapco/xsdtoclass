671aca86171f0c3f9015bcbf04442a9a674fbfc3
marcelo <marcelogaguero@hotmail.com>
2013-08-28 16:23:22 -0300

    - mas cambios

---
+++ src/Constants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by WIKDOS.
+ * @author: Marcelo Agüero <marcelogaguero@hotmail.com>
+ * @since: 27/08/13 09:51
+ *
+ */
+namespace src;
+
+class Constants
+{
+    const COMPLEX = 'complexType';
+    const SEQUENCE = 'sequence';
+    const ELEMENT = 'element';
+    const SIMPLE = 'simpleContent';
+    const ATTRIBUTE = 'attribute';
+    const EXTENSION = 'extension';
+
+}
--- src/XsdToClass.php
+++ src/XsdToClass.php
@@ -7,17 +7,21 @@
  */
 namespace src;
 
-use src\exceptions\XsdToClassException;
-
 require_once(__DIR__."/exceptions/XsdToClassException.php");
+require_once(__DIR__."/Constants.php");
+
+use src\exceptions\XsdToClassException;
+use src\Constants;
 
 class XsdToClass
 {
     /** @var \SimpleXMLElement $xsd */
     protected $xsd;
+    protected $folder;
 
     function __construct($xsd){
         if (file_exists($xsd)) {
+            $this->folder = dirname(realpath($xsd));
             $xml = simplexml_load_file($xsd);
             $this->xsd = $xml;
         } else {
@@ -25,26 +29,45 @@ class XsdToClass
         }
     }
 
-    public function createFileClass($className){
-
+    public function createFileClass($className, $elements){
+        foreach($elements as $element){
+            switch ($element->nodeName) {
+                case Constants::COMPLEX:
+                    die(var_dump($element));
+                break;
+                default:
+            }
+        }
     }
 
     public function convertXsd(){
         try {
             $includes = $this->xsd->xpath('/xs:schema/xs:include');
-
             foreach($includes as $include){
                 $file = $include->attributes()->schemaLocation;
-                $xsdToClass = new XsdToClass($file);
-                // $require = $xsdToClass->convertXsd();
+                try {
+                    $xsdToClass = new XsdToClass($this->folder.DIRECTORY_SEPARATOR.$file);
+                    $require = $xsdToClass->convertXsd();
+                    die(var_dump($require));
+                } catch (\Exception $e){
+                    throw new XsdToClassException($e->getMessage());
+                }
             }
 
-            $elements = $this->xsd->xpath('/xs:schema/xs:element');
+            $nodes = array();
+            $elements = $this->xsd->xpath('//xs:schema/descendant::*');
+
             foreach($elements as $element){
-                $this->createFileClass($className);
+                $obj = new \stdClass();
+                $obj->nodeName = $element->getName();
+                $obj->name = (string) $element->attributes()->name;
+                $obj->type = (string) $element->attributes()->type;
+                $obj->xml = $element;
+                $nodes[] = $obj;
             }
 
-            die(var_dump($elements));
+            $this->createFileClass($this->xsd->getName(), $nodes);
+
 
         } catch(\Exception $e){
             throw new XsdToClassException($e->getMessage());
